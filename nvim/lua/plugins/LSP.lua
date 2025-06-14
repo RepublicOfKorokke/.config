8613fa5467583d53f07e0e2216c8c441364097fa
RepublicOfKorokke <republicofkorokke@users.noreply.github.com>
2025-06-15 00:00:04 +0900

    Update config for mason-lsp ver 2.0.0

--- nvim/lua/plugins/LSP.lua
+++ nvim/lua/plugins/LSP.lua
@@ -15,25 +15,6 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = { "lua_ls", "rust_analyzer", "pyright" },
 		})
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = require("cmp_nvim_lsp").default_capabilities(),
-				})
-				vim.notify("Set LSP by mason: " .. server_name)
-			end,
-			["lua_ls"] = function()
-				lspconfig.lua_ls.setup({
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				})
-			end,
-		})
 		require("fidget").setup({})
 	end,
 }
