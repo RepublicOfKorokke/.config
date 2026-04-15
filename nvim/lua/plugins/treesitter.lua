0d800e6586e177e74956097341a66f1ba00ab733
RepublicOfKorokke <republicofkorokke@users.noreply.github.com>
2026-04-15 13:41:21 +0900

    nvim: add nvim-treesitter-context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -21,8 +21,12 @@ return {
 				-- [options]
 			},
 		})
+		vim.keymap.set("n", "[c", function()
+			require("treesitter-context").go_to_context(vim.v.count1)
+		end, { silent = true })
 	end,
 	dependencies = {
 		"HiPhish/rainbow-delimiters.nvim",
+		"nvim-treesitter/nvim-treesitter-context",
 	},
 }
