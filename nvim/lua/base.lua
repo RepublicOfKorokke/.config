9fd0ffb5dc84da7adf5ed69cdc366915ec3adc0d
RepublicOfKorokke <republicofkorokke@users.noreply.github.com>
2026-05-04 01:13:39 +0900

    nvim: add Unicode2Text command

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -5,3 +5,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.api.nvim_create_user_command("FormatJson", "%!jq '.'", {})
+vim.api.nvim_create_user_command('Unicode2Text', function()
+  vim.cmd([[:%s/\\u\(\x\{4\}\)/\=nr2char(str2nr(submatch(1), 16))/g]])
+end, { desc = 'Convert Unicode escape sequences to text' })
+
